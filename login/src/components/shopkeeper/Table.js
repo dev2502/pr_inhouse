c74e2c8160d3a5d2b1b93e4a2438fb29b7f68616
Ayu <ayushi.30chn@gmail.com>
2023-04-11 13:21:22 +0530

    updateleft

--- login/src/components/shopkeeper/Table.js
+++ login/src/components/shopkeeper/Table.js
@@ -1,239 +1,273 @@
-import React, { useState ,useRef } from 'react';
-import Data from './data.json';
-import '../../headerForAll.css';
-import logo2 from '../images/logo2.png';
-import bv_logo from '../images/bv_logo.jpg';
-import './table.css';
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from "react";
+import Data from "./data.json";
+import "../../headerForAll.css";
+import logo2 from "../images/logo2.png";
+import bv_logo from "../images/bv_logo.jpg";
+import "./table.css";
+import axios from "axios";
 
 function Table() {
-    const [datas, setData] = useState(Data)
- const [editState, setEditState] = useState(-1)
-    return ( 
-        <>
-        {/* HEADER */}
-       <div className="header">
-          <div className="logo">
-            <img src={logo2} alt="Logo"/>
-          </div>
-          <div className="bv_logo">
-            <img src={bv_logo} alt="Logo"/>
-          </div>
-       </div>
-        <div className='tableWrap'>
-            <div>
-                <AddItem setData={setData} />
-                <form onSubmit={handleUpdate}>
-                <table className="td1" border="2">
-                <th>PRODUCT ID </th>  
-                <th>PNAME </th>
-                <th>PRICE </th>
-                <th>QTY </th>
-                <th>ACTION </th>
+  const [datas, setData] = useState(Data);
+  const [editState, setEditState] = useState(-1);
+  const [products, setProducts]= useState([]);
+  useEffect(() => {
+    console.log("product in use effect",products)
+   axios.get("http://localhost:9002/api/all-products")
+   .then((res) => setProducts(res.data))
+  },[])
+  console.log("out",products)
 
-                {datas.map((current) => (
-                    editState === current.id ? <EditItem  current={current} datas={datas} setData={setData}/> :
-                    <tr key={current.id}>
-                        <td>{current.pid}</td>
-                        <td>{current.pname}</td>
-                        <td>{current.price}</td>
-                        <td>{current.qty}</td>
-                        <td>
-                            <button className="edit" onClick={() => handleEdit(current.id)}> Edit</button>
-                            <button className="delete" onClick={() => handleDelete(current.id)} >Delete</button>
-                        </td>
-                    </tr>
-                ))}
-                    </table>
-                 </form>
-            </div>
+  return (
+    <>
+      {/* HEADER */}
+      <div className="header">
+        <div className="logo">
+          <img src={logo2} alt="Logo" />
         </div>
-        </>
-    )
-    function handleUpdate(event) {
-      event.preventDefault();
-        const pid = event.target.elements.pid.value;
-      const pname = event.target.elements.pname.value;
-    const price = event.target.elements.price.value;
-    const qty = event.target.elements.qty.value;
-    const updatedData = datas.map(d => d.id === editState ? {...d, pid:pid, pname:pname, price:price, qty:qty }:d)
-    setEditState(-1)
-    setData(updatedData)
-          
-    }
-    function handleEdit(id) {
-        setEditState(id)
-    }
-    function handleDelete(id) {
-      const updatedData = datas.filter((d) => id!==d.id  )
-      setData(updatedData)
+        <div className="bv_logo">
+          <img src={bv_logo} alt="Logo" />
+        </div>
+      </div>
+      <div className="tableWrap">
+        <div>
+          <AddItem setData={setData} />
+          <form>
+            <table className="td1" border="2">
+              <th>PRODUCT ID </th>
+              <th>PNAME </th>
+              <th>PRICE </th>
+              {products?.map((current) =>
+                editState === current.productId ? (
+                  <EditItem current={current} datas={datas} setData={setData} />
+                ) : (
+                  <tr key={current.productId}>
+                    <td>{current.productId}</td>
+                    <td>{current.productName}</td>
+                    <td>{current.productPrice}</td>
+                    <td>{current.productQuantity}</td>
+                    <td>
+                      <button
+                        className="edit"
+                        onClick={() => handleEdit(current.productId)}
+                      >
+                        {" "}
+                        Edit
+                      </button>
+                      <button
+                        className="delete"
+                        onClick={() => handleDelete(current.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )}
+              </table>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+  // function handleUpdate(id) {
+  //  console.log("update",id)
+  // //  axios.put(`http://localhost:9002/api/products/${id}`)
+  // }
+  function handleEdit(id) {
+    setEditState(id);
   }
-    
+  function handleDelete(id) {
+    console.log(id)
+   axios.delete(`http://localhost:9002/api/products/${id}`)
+  }
 }
-
-
-function EditItem({current, datas,setData}) {
-    function handlePid(event)
-    {
-        const pid = event.target.value;
-        const regex = /^p\d+$/;
+function EditItem({ current, datas, setData }) {
+  function handlePid(event) {
+    const pid = event.target.value;
+    const regex = /^p\d+$/;
     if (regex.test(pid)) {
       setData(pid);
     }
-        const updatedData = datas.map((d) => d.id === current.id ? {...d, pid:pid} : d)
-        setData(updatedData)
+    const updatedData = datas.map((d) =>
+      d.id === current.id ? { ...d, pid: pid } : d
+    );
+    setData(updatedData);
+  }
+  function handlePname(event) {
+    const pname = event.target.value;
+    const updatedData = datas.map((d) =>
+      d.id === current.id ? { ...d, pname: pname } : d
+    );
+    setData(updatedData);
+  }
+  function handleUpdate(id,name,quantity,price) {
+    console.log("update",id)
+    axios.put(`http://localhost:9002/api/products/${id}`,{
+      name,
+      quantity,
+      price,
     }
-    function handlePname(event)
-    {
-        const pname = event.target.value;
-        const updatedData = datas.map((d) => d.id === current.id ? {...d, pname:pname } : d)
-        setData(updatedData)
-    }
-    function handlePrice(event)
-    {
-        const price = event.target.value;
-        const updatedData = datas.map((d) => d.id === current.id ? {...d, price:price } : d)
-        setData(updatedData)
-    }
-    function handleQty(event)
-    {
-        const qty = event.target.value;
-        const updatedData = datas.map((d) => d.id === current.id ? {...d, qty:qty } : d)
-        setData(updatedData)
-    }
-    return(
-       <tr> 
-            <td><input   type="text" onChange={handlePid} value={current.pid} name="pid" placeholder="Enter id" required /></td>
-            <td> <input type="text"  onChange={handlePname} value={current.pname} name="pname" placeholder="Enter name" required/></td>
-            <td><input type="number" onChange={handlePrice} value={current.price} name="price"placeholder="Enter price" required /></td>
-            <td><input type="number" onChange={handleQty} value={current.qty} name="qty" placeholder="Enter qty" required/></td>
-            <td><button type="submit" >Update</button></td>
-       </tr> 
-
-    )
+    ).then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+   }
+  function handlePrice(event) {
+    const price = event.target.value;
+    const updatedData = datas.map((d) =>
+      d.id === current.id ? { ...d, price: price } : d
+    );
+    setData(updatedData);
+  }
+  function handleQty(event) {
+    const qty = event.target.value;
+    const updatedData = datas.map((d) =>
+      d.id === current.id ? { ...d, qty: qty } : d
+    );
+    setData(updatedData);
+  }
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          onChange={handlePid}
+          value={current.pid}
+          name="pid"
+          placeholder="Enter id"
+          required
+        />
+      </td>
+      <td>
+        {" "}
+        <input
+          type="text"
+          onChange={handlePname}
+          value={current.pname}
+          name="pname"
+          placeholder="Enter name"
+          required
+        />
+      </td>
+      <td>
+        <input
+          type="number"
+          onChange={handlePrice}
+          value={current.price}
+          name="price"
+          placeholder="Enter price"
+          required
+        />
+      </td>
+      <td>
+        <input
+          type="number"
+          onChange={handleQty}
+          value={current.qty}
+          name="qty"
+          placeholder="Enter qty"
+          required
+        />
+      </td>
+      <td>
+      <button type="submit" onClick={() => handleUpdate(current.pid,current.name,current.qty,current.price)}>Update</button>
+      </td>
+    </tr>
+  );
 }
-function AddItem({setData}){
-    const pidRef = useRef()
-    const pnameRef = useRef()
-    const priceRef = useRef()
-    const qtyRef = useRef()
-
-    function handleValues(event)
-    {
-      event.preventDefault();
-      const pid = event.target.elements.pid.value;
-      const pname = event.target.elements.pname.value;
+function AddItem({ setData }) {
+  const pidRef = useRef();
+  const pnameRef = useRef();
+  const priceRef = useRef();
+  const qtyRef = useRef();
+  function handleValues(event) {
+    event.preventDefault();
+    const pid = event.target.elements.pid.value;
+    const pname = event.target.elements.pname.value;
     const price = event.target.elements.price.value;
     const qty = event.target.elements.qty.value;
     const newItem = {
-        id:4,
-        pid, 
-        pname,
-        price,
-        qty,
-    }
-    setData(prevData => prevData.concat(newItem))
-    pidRef.current.value =""
-    pnameRef.current.value =""
-    priceRef.current.value =""
-    qtyRef.current.value =""
+      id: 4,
+      pid,
+      pname,
+      price,
+      qty,
+    };
+    setData((prevData) => prevData.concat(newItem));
+    pidRef.current.value = "";
+    pnameRef.current.value = "";
+    priceRef.current.value = "";
+    qtyRef.current.value = "";
+  }
+  const [product, setProduct] = useState({
+    id: "",
+    name: "",
+    quantity: "",
+    price: "",
+  });
 
-}
-const [product, setProduct] = useState({
-  id: '',
-  name: '',
-  quantity: '',
-  price: '',
-});
-
-const handleChange = (e) => {
-  setProduct({ ...product, [e.target.name]: e.target.value });
-};
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-  
-//   axios.post('http://localhost:9002/api/products', product)
-//     .then((res) => {
-//       console.log(res.data);
-//       setProduct({
-//         id: '',
-//         name: '',
-//         quantity: '',
-//         price: '',
-//       });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  axios
-    .post('http://localhost:9002/Table', product)
-    .then((res) => {
-      console.log(res.data);
-      setProduct({
-        id: '',
-        name: '',
-        quantity: '',
-        price: '',
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("product", product);
+    axios
+      .post("http://localhost:9002/api/products", product)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
-const login = () => {
-  axios
-    .post("http://localhost:9002/login", user)
-    .then((res) => {
-      alert(res.data.message);
-    })
-    .catch((error) => console.log(error));
-    navigate("/Student_homepage");
-};
-
-// const register = () => {
-//   const { name, SmartId, password, reEnterPassword } = user
-//   if( name && SmartId && password && password === reEnterPassword){
-//        axios.post("http://localhost:9002/register", user)
-//        .then( res => console.log(res))
-//        navigate("/login")
-//   }else{
-//       alert("invalid!");
-//       return false;
-//   }
-// }
-
-
-    return(
-        <>     
-        <form>
-      <label>
-        ID:
-        <input type="text" name="id" value={product.id} onChange={handleChange} />
-      </label>
-      <label>
+ };
+  return (
+    <>
+      <form>
+        <label>
+          ID:
+          <input
+            type="text"
+            name="id"
+            value={product.id}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
         Name:
-        <input type="text" name="name" value={product.name} onChange={handleChange} />
-      </label>
-      <label>
-        Quantity:
-        <input type="text" name="quantity" value={product.quantity} onChange={handleChange} />
-      </label>
-      <label>
-      Price:
-        <input type="text" name="price" value={product.price} onChange={handleChange} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Add Product</button>
-    </form>
+          <input
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Quantity:
+          <input
+            type="text"
+            name="quantity"
+            value={product.quantity}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+        Price:
+          <input
+            type="text"
+            name="price"
+            value={product.price}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit" onClick={handleSubmit}>
+          Add Product
+        </button>
+      </form>
     </>
-    
-    )
-    }
-
-
-export default Table;
+    );
+  }
+  
+  export default Table;
--- server_login/index.js
+++ server_login/index.js
@@ -1,172 +1,166 @@
-const express = require('express');
-const cors= require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-main().catch(err => console.log(err));
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+main().catch((err) => console.log(err));
 
 async function main() {
-    mongoose.set('strictQuery', true)
-    await mongoose.connect('mongodb://127.0.0.1:27017/goeazydb');
-    console.log('db connected')
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  }
-  const ShopkeeperSchema = new mongoose.Schema({
-    Shopname: String,
-    ShopId: String,
-    Spassword: String
-  });
+mongoose.set("strictQuery", true);
+await mongoose.connect("mongodb://127.0.0.1:27017/goeazydb");
+console.log("db connected");
+// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+const ShopkeeperSchema = new mongoose.Schema({
+Shopname: String,
+ShopId: String,
+Spassword: String,
+});
 
-  const Shopkeepers = mongoose.model('Shopkeepers', ShopkeeperSchema);
-  
-  const StudentSchema = new mongoose.Schema({
-   name: String,
-    SmartId: String,
-    password: String
-  });
+const Shopkeepers = mongoose.model("Shopkeepers", ShopkeeperSchema);
 
-  const Students = mongoose.model('Students', StudentSchema);
+const StudentSchema = new mongoose.Schema({
+name: String,
+SmartId: String,
+password: String,
+});
 
-  const productSchema = new mongoose.Schema({
-    productId: { type: String, required: true },
-    productName: { type: String, required: true },
-    productQuantity: { type: Number, required: true },
-    productPrice: { type: Number, required: true },
-  });
+const Students = mongoose.model("Students", StudentSchema);
 
-  const Product = mongoose.model("Product", productSchema);
+const productSchema = new mongoose.Schema({
+productId: { type: String, required: true },
+productName: { type: String, required: true },
+productQuantity: { type: Number, required: true },
+productPrice: { type: Number, required: true },
+});
 
-  const server = express();
+const Product = mongoose.model("Product", productSchema);
 
-  server.use(cors());
-  server.use(bodyParser.json()); 
+const server = express();
 
+server.use(cors());
+server.use(bodyParser.json());
 
 //student login
-server.post('/login', (req,res)=> {
-    const { SmartId, password } = req.body
-    Students.findOne({ SmartId: SmartId}, (err, user) => {
-        if(user){
-            if(password === user.password){
-                res.send({message: "login successfull", user: user})
-            }else{
-                res.send({message: "password do not match"})
-            }
-        }else{
-            res.send({message: "user not registered"})
-            
-        }
-    })
-})
+server.post("/login", (req, res) => {
+const { SmartId, password } = req.body;
+Students.findOne({ SmartId: SmartId }, (err, user) => {
+if (user) {
+if (password === user.password) {
+res.send({ message: "login successfull", user: user });
+} else {
+res.send({ message: "password do not match" });
+}
+} else {
+res.send({ message: "user not registered" });
+}
+});
+});
 
 //shopkeeper login
-server.post('/Shopkeeper_login', (req,res)=> {
-  const { ShopId, Spassword } = req.body
-  Shopkeepers.findOne({ ShopId: ShopId}, (err, shop) => {
-      if(shop){
-          if(Spassword === shop.Spassword){
-              res.send({message: "login successfull", shop: shop})
-          }else{
-              res.send({message: "password do not match"})
-          }
-      }else{
-          res.send({message: "shop not registered"})
-          
-      }
-  })
-})
+server.post("/Shopkeeper_login", (req, res) => {
+const { ShopId, Spassword } = req.body;
+Shopkeepers.findOne({ ShopId: ShopId }, (err, shop) => {
+if (shop) {
+if (Spassword === shop.Spassword) {
+res.send({ message: "login successfull", shop: shop });
+} else {
+res.send({ message: "password do not match" });
+}
+} else {
+res.send({ message: "shop not registered" });
+}
+});
+});
 //register
-server.post('/register', (req,res)=> {
-    const {name, SmartId, password}= req.body
-    Students.findOne({SmartId: SmartId}, (err, user) => {
-        if(user){
-            res.send({ message: "user already registered!"})
-        }
-        else{
-            const user = new Students({
-                name,
-                SmartId,
-                password
-           })
-           user.save(err => {
-               if(err){
-                   res.send(err)
-               }else{
-                   res.send( { message: "Successfully Registered" })
-               }
-           })
-        }
-    })
-    
-})
-
+server.post("/register", (req, res) => {
+const { name, SmartId, password } = req.body;
+Students.findOne({ SmartId: SmartId }, (err, user) => {
+if (user) {
+res.send({ message: "user already registered!" });
+} else {
+const user = new Students({
+name,
+SmartId,
+password,
+});
+user.save((err) => {
+if (err) {
+res.send(err);
+} else {
+res.send({ message: "Successfully Registered" });
+}
+});
+}
+});
+});
 
 // Create a products
-server.post("/Table", (req, res) => {
-  const product = new Product({
-    id: req.body.id,
-    name: req.body.name,
-    quantity: req.body.quantity,
-    price: req.body.price,
-  });
+server.post("/api/products", (req, res) => {
+console.log("req", req);
+const product = new Product({
+productId: req.body.id,
+productName: req.body.name,
+productQuantity: req.body.quantity,
+productPrice: req.body.price,
+});
 
-  product.save((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error adding product!");
-    } else {
-      res.send("Product added successfully!");
-    }
-  });
+product.save((err) => {
+if (err) {
+console.error(err);
+res.status(500).send("Error adding product!");
+} else {
+res.send("Product added successfully!");
+}
+});
 });
 
 // Update a product
 server.put("/api/products/:id", (req, res) => {
-    const id = req.params.id;
-  
-    Product.findOneAndUpdate(
-      { id },
-      {
-        name: req.body.name,
-        quantity: req.body.quantity,
-        price: req.body.price,
-      },
-      (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error updating product!");
-        } else {
-          res.send("Product updated successfully!");
-        }
-      }
-    );
-  });
+const id = req.params.id;
 
-  // Delete a products
+Product.findOneAndUpdate(
+{ id },
+{
+name: req.body.name,
+quantity: req.body.quantity,
+price: req.body.price,
+},
+(err) => {
+if (err) {
+console.error(err);
+res.status(500).send("Error updating product!");
+} else {
+res.send("Product updated successfully!");
+}
+}
+);
+});
+
+// Delete a products
 server.delete("/api/products/:id", (req, res) => {
-    const id = req.params.id;
-  
-    Product.deleteOne({ id }, (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error deleting product!");
-      } else {
-        res.send("Product deleted successfully!");
-      }
-    });
-  });
+const id = req.params.id;
 
-  // Get all products
+Product.deleteOne({ id }, (err) => {
+if (err) {
+console.error(err);
+res.status(500).send("Error deleting product!");
+} else {
+res.send("Product deleted successfully!");
+}
+});
+});
+
+// Get all products
 server.get("/api/all-products", (req, res) => {
-    Product.find({}, (err, products) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(products);
-      }
-    });
-  });
+Product.find({}, (err, products) => {
+if (err) {
+res.status(500).send(err);
+} else {
+res.status(200).send(products);
+}
+});
+});
 
-server.listen(9002,()=> {
-    console.log("started at port 9002")
-})
-
+server.listen(9002, () => {
+console.log("started at port 9002");
+});
